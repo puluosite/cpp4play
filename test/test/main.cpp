a6d8639281f4a4590e1e140ac44718a14f9f2329
Tianheng-laptop <mynameisprost@gmail.com>
2015-07-02 16:13:58 -0700

    shared_ptr dynamic_cast experiments

--- test/test/main.cpp
+++ test/test/main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
 
 class Base {
 public:
+	virtual ~Base() {cout<<"base dtor"<<endl;}
+
 	int healthVal() const {
 		cout<<"base pre"<<endl;
 		int a = healthValImp();
 		cout<<"base post"<<endl;
 		return a;
 	}
+	
 
 protected:
 	virtual int healthValImp() const {
@@ -21,7 +25,8 @@ protected:
 
 class Derive: public Base {
 public:
-	
+	virtual ~Derive() {cout<<"derive dtor"<<endl;}
+
 	int healthVal() const {
 		cout<<"derive pre"<<endl;
 		int a = Base::healthValImp();
@@ -40,9 +45,55 @@ protected:
 };
 
 int main() {
+	/*
 	Derive d;
 	d.healthVal();
 	Base b;
 	b.healthVal();
+	*/
+
+	
+	shared_ptr<Base> pb1(new Derive());
+	shared_ptr<Base> pb2(new Base());
+
+	if (shared_ptr<Derive>pt = dynamic_pointer_cast<Derive>(pb1)) {
+		cout<<"pb1 is a base pointer point to DERIVE class"<<endl;
+	} else {
+		cout<<"pb1 is a base pointer point to BASE class"<<endl;
+	}
+
+	if (shared_ptr<Derive>pt = dynamic_pointer_cast<Derive>(pb2)) {
+		cout<<"pb2 is a base pointer point to DERIVE class"<<endl;
+	} else {
+		cout<<"pb2 is a base pointer point to BASE class"<<endl;
+	}
+
+
+
+
+	/*
+	Base* pb1 = new Derive();
+	Base* pb2 = new Base();
+
+	if (Derive* pd = dynamic_cast<Derive*>(pb1)) {
+		cout<<"pb1 is a base pointer point to DERIVE class"<<endl;
+	} else {
+		cout<<"pb1 is a base pointer point to BASE class"<<endl;
+	}
+
+	if (Derive* pd = dynamic_cast<Derive*>(pb2)) {
+		cout<<"pb2 is a base pointer point to DERIVE class"<<endl;
+	} else {
+		cout<<"pb2 is a base pointer point to BASE class"<<endl;
+	}
+
+	
+	delete pb1;
+	delete pb2;
+	*/
+	
+
+	
+
 	return 0;
 }
